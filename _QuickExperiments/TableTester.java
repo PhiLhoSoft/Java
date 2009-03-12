083c51cf6b2cbc8706bf00509b85d55212183335
plhoste <Philippe.Lhoste@Mycom-int.com>
2009-03-12 19:01:24 +0100

    Adding JTable tester

---
+++ _QuickExperiments/TableTester.java
@@ -0,0 +1,106 @@
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+
+public class TableTester extends JPanel {
+    public TableTester() {
+        super(new GridLayout(1,0));
+
+        final String[] columnNames = {"First Name",
+                                      "Last Name",
+                                      "Sport",
+                                      "# of Years",
+                                      "Vegetarian"};
+
+        final Object[][] data = {
+            {"Tom",   "Roberts","Athletic", new Integer(5),  new Boolean(false)},
+            {"Sarah", "Watt",   "Football", new Integer(3),  new Boolean(true)},
+            {"Laura", "Brown",  "Swimming", new Integer(2),  new Boolean(false)},
+            {"Simon", "Smith",  "Tennis",   new Integer(20), new Boolean(true)},
+            {"Paul",  "Jones",  "Rugby",    new Integer(10), new Boolean(false)},
+            {"Paul 1",  "Jones",  "Rugby",    new Integer(10), new Boolean(false)},
+            {"Paul 2",  "Jones",  "Rugby",    new Integer(10), new Boolean(false)},
+            {"Paul 3",  "Jones",  "Rugby",    new Integer(10), new Boolean(false)},
+            {"Paul 4",  "Jones",  "Rugby",    new Integer(10), new Boolean(false)}
+       };
+
+        JTable table = new SpecialTable(data, columnNames);
+        table.setPreferredScrollableViewportSize(new Dimension(500, 100));
+/*
+        table.addMouseListener(new MouseListener(){
+            public void mouseEntered(MouseEvent me){}
+            public void mouseExited(MouseEvent me){}
+            public void mouseReleased(MouseEvent me){}
+            public void mouseClicked(MouseEvent me){}
+            public void mousePressed(MouseEvent me){
+                if (me.isControlDown()){
+                    System.out.println("This is working ");
+                }
+            }
+        });
+*/
+        InputMap inputMap = table.getInputMap(JTable.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        KeyStroke keyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.CTRL_MASK);
+        inputMap.put(keyStroke, "none");
+
+        JScrollPane scrollPane = new JScrollPane(table);
+        add(scrollPane);
+    }
+
+    private static void createAndShowGUI() {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame("TableTester");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        TableTester newContentPane = new TableTester();
+        newContentPane.setOpaque(true);
+        frame.setContentPane(newContentPane);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
+
+class SpecialTable extends JTable
+{
+    boolean bIsControlDown;
+    int clickedRow;
+    
+    SpecialTable(Object[][] data, String[] columnNames)
+    {
+        super(data, columnNames);
+//        setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        getSelectionModel().addListSelectionListener(this);
+        addMouseListener(new MouseInputAdapter()
+        {
+            public void mousePressed(MouseEvent me)
+            {
+                bIsControlDown = me.isControlDown();
+                clickedRow = rowAtPoint(me.getPoint());
+            }
+        });
+    }
+    
+    public void valueChanged(ListSelectionEvent evt)  
+    {
+        super.valueChanged(evt);
+        if (bIsControlDown)
+        {
+            if (!evt.getValueIsAdjusting())
+            {
+//                System.out.println(evt);
+//                System.out.println("=> " + clickedRow);
+                getSelectionModel().removeSelectionInterval(clickedRow, clickedRow);
+            }
+        }
+    }
+}
