83f2d55ab339ce9ef8c91d348681b799bdf05457
PhiLho <PhiLho@GMX.net>
2009-05-21 16:35:25 +0200

    Changing indents in RunApp

--- _QuickExperiments/RunApp/ETLAppDesc.java
+++ _QuickExperiments/RunApp/ETLAppDesc.java
@@ -27,103 +27,103 @@ exec nice -n 19 $javaBin/java -Detl-dimg -server -$XmsDIMgr -$XmxDIMgr $(COMMON_
  */
 public enum ETLAppDesc
 {
-   ETL_DIMGR("DIMgr",    "DIMG", "com.mycom.etl.DIMgr",
-         new String[] { "-Detl-dimg" },
-         new String[] { "-C", "?c" }
-   ),
-   ETL_EXTR("Extract",   "EXTR", "com.mycom.etl.extr.ExtractApp",
-         new String[] { "-Detl-extr" },
-         new String[] { "-C", "?c" }
-   ),
-   ETL_TRNF("Transform", "TRNF", "com.mycom.etl.trf.TransformApp",
-         new String[] { "-Detl-trnf" },
-         new String[] { "-C", "?c" }
-   ),
-   ETL_LOAD("Load",      "LOAD", "com.mycom.etl.load.LoadApp",
-         new String[] { "-Detl-load" },
-         new String[] { "-C", "?c" }
-   ),
-   ETL_WRIT("Write",     "WRIT", "com.mycom.etl.write.WriteApp",
-         new String[] { "-Detl-writ" },
-         new String[] { "-C", "?c" }
-   );
+	ETL_DIMGR("DIMgr",	 "DIMG", "com.mycom.etl.DIMgr",
+			new String[] { "-Detl-dimg" },
+			new String[] { "-C", "?c" }
+	),
+	ETL_EXTR("Extract",	"EXTR", "com.mycom.etl.extr.ExtractApp",
+			new String[] { "-Detl-extr" },
+			new String[] { "-C", "?c" }
+	),
+	ETL_TRNF("Transform", "TRNF", "com.mycom.etl.trf.TransformApp",
+			new String[] { "-Detl-trnf" },
+			new String[] { "-C", "?c" }
+	),
+	ETL_LOAD("Load",		"LOAD", "com.mycom.etl.load.LoadApp",
+			new String[] { "-Detl-load" },
+			new String[] { "-C", "?c" }
+	),
+	ETL_WRIT("Write",	  "WRIT", "com.mycom.etl.write.WriteApp",
+			new String[] { "-Detl-writ" },
+			new String[] { "-C", "?c" }
+	);
 
-   private final String m_strName;
-   private final String m_strThreadName;
-   private final String m_strClass;
-   private final String[] m_straVMArgs;
-   private final String[] m_straProgramArgs;
+	private final String m_strName;
+	private final String m_strThreadName;
+	private final String m_strClass;
+	private final String[] m_straVMArgs;
+	private final String[] m_straProgramArgs;
 
-   private static final String[] s_straCommonVMArgs =
-   {
-      "-Dsun.rmi.transport.tcp.handshakeTimeout=300000",
-      "-Dsun.rmi.dgc.client.gcInterval=900000",
-      "-Dsun.rmi.dgc.server.gcInterval=900000",
-      "-XX:+UseParallelGC",
-      "-XX:+AggressiveHeap",
-      "-DMomMessageClass=org.objectweb.joram.shared.messages.MessageSoftRef",
-      "-XX:GCHeapFreeLimit=5",
+	private static final String[] s_straCommonVMArgs =
+	{
+		"-Dsun.rmi.transport.tcp.handshakeTimeout=300000",
+		"-Dsun.rmi.dgc.client.gcInterval=900000",
+		"-Dsun.rmi.dgc.server.gcInterval=900000",
+		"-XX:+UseParallelGC",
+		"-XX:+AggressiveHeap",
+		"-DMomMessageClass=org.objectweb.joram.shared.messages.MessageSoftRef",
+		"-XX:GCHeapFreeLimit=5",
 
-//      "-server",
-      "-Djava.awt.headless=true"
-   };
-   private static final String[] s_straCommonProgramArgs =
-   {
-      "RMI.Server=localhost",
-      "RMI.Port=1099"
-   };
+//		"-server",
+		"-Djava.awt.headless=true"
+	};
+	private static final String[] s_straCommonProgramArgs =
+	{
+		"RMI.Server=localhost",
+		"RMI.Port=1099"
+	};
 
-   ETLAppDesc(String strName, String strThreadName, String strClass,
-         String[] straVMArgs, String[] straProgramArgs)
-   {
-      m_strName = strName;
-      m_strThreadName = strThreadName;
-      m_strClass = strClass;
-      m_straVMArgs = straVMArgs;
-      m_straProgramArgs = straProgramArgs;
-   }
+	ETLAppDesc(String strName, String strThreadName, String strClass,
+			String[] straVMArgs, String[] straProgramArgs)
+	{
+		m_strName = strName;
+		m_strThreadName = strThreadName;
+		m_strClass = strClass;
+		m_straVMArgs = straVMArgs;
+		m_straProgramArgs = straProgramArgs;
+	}
 
-   public String GetName()
-   {
-      return m_strName;
-   }
+	public String GetName()
+	{
+		return m_strName;
+	}
 
-   public String GetThreadName()
-   {
-      return m_strThreadName;
-   }
+	public String GetThreadName()
+	{
+		return m_strThreadName;
+	}
 
-   public String GetClass()
-   {
-      return m_strClass;
-   }
+	public String GetClass()
+	{
+		return m_strClass;
+	}
 
-   public String[] GetVMArgs()
-   {
-      return m_straVMArgs;
-   }
-   public String[] GetProgramArgs()
-   {
-      return m_straProgramArgs;
-   }
+	public String[] GetVMArgs()
+	{
+		return m_straVMArgs;
+	}
+	public String[] GetProgramArgs()
+	{
+		return m_straProgramArgs;
+	}
 
-   public static String[] GetCommonVMArgs()
-   {
-      return s_straCommonVMArgs;
-   }
-   public static String[] GetCommonProgramArgs()
-   {
-      return s_straCommonProgramArgs;
-   }
+	public static String[] GetCommonVMArgs()
+	{
+		return s_straCommonVMArgs;
+	}
+	public static String[] GetCommonProgramArgs()
+	{
+		return s_straCommonProgramArgs;
+	}
 
-   // Test function
-   public static void main(String[] args)
-   {
-      for (ETLAppDesc ea : ETLAppDesc.values())
-      {
-         System.out.printf("ETL App Desc: %s (%s) = %d",
-               ea, ea.name(), ea.ordinal());
-      }
-   }
+	// Test function
+	public static void main(String[] args)
+	{
+		for (ETLAppDesc ea : ETLAppDesc.values())
+		{
+			System.out.printf("ETL App Desc: %s (%s) = %d",
+					ea, ea.name(), ea.ordinal());
+		}
+	}
 }
 
--- _QuickExperiments/RunApp/StreamGobbler.java
+++ _QuickExperiments/RunApp/StreamGobbler.java
@@ -13,47 +13,47 @@ import java.io.InputStreamReader;
  */
 public class StreamGobbler extends Thread
 {
-   InputStream m_is;
-   String m_type;
-   String m_processName;
+	InputStream m_is;
+	String m_type;
+	String m_processName;
 
-   public StreamGobbler( InputStream is, String processName )
-   {
-      m_is = is;
-      m_type = "";
-      m_processName = processName;
-   }
+	public StreamGobbler( InputStream is, String processName )
+	{
+		m_is = is;
+		m_type = "";
+		m_processName = processName;
+	}
 
-   public StreamGobbler( InputStream is, String type, String processName )
-   {
-      m_is = is;
-      m_type = type;
-      m_processName = processName;
-   }
+	public StreamGobbler( InputStream is, String type, String processName )
+	{
+		m_is = is;
+		m_type = type;
+		m_processName = processName;
+	}
 
-   public void run()
-   {
-      InputStreamReader isr = new InputStreamReader( m_is );
-      BufferedReader br = new BufferedReader( isr );
-      String line = null;
-      try
-      {
-         while ( ( line = br.readLine()) != null )
-         {
-            HandleOutputLine( line );
-         }
-      }
-      catch ( IOException ioe )
-      {
-         ioe.printStackTrace();
-      }
-   }
+	public void run()
+	{
+		InputStreamReader isr = new InputStreamReader( m_is );
+		BufferedReader br = new BufferedReader( isr );
+		String line = null;
+		try
+		{
+			while ( ( line = br.readLine()) != null )
+			{
+				HandleOutputLine( line );
+			}
+		}
+		catch ( IOException ioe )
+		{
+			ioe.printStackTrace();
+		}
+	}
 
-   public String HandleOutputLine( String line )
-   {
-      // Default implementation, to override
-      String l = m_processName + " " + m_type + "> " + line;
-      System.out.println( l );
-      return l;
-   }
+	public String HandleOutputLine( String line )
+	{
+		// Default implementation, to override
+		String l = m_processName + " " + m_type + "> " + line;
+		System.out.println( l );
+		return l;
+	}
 }
