fff7cdd45f66d8d7ecb02dac0193cea5fdee6264
PhiLho <PhiLho@GMX.net>
2009-09-18 19:15:12 +0200

    Perspective transform draft

--- _QuickExperiments/DrJavaTest.java
+++ _QuickExperiments/DrJavaTest.java
@@ -5,8 +5,20 @@ public class DrJavaTest
     private String name = "instance name";
     private static String staticName = "static name";
 
-    public static void main(String args[]) 
+    public static void main(String args[])
     {
       System.out.println(Locale.getDefault().getCountry());
+      Foo foo = new Foo()
+      {
+        int af(int y) { return x * y; }
+      };
     }
-} 
+}
+
+abstract class Foo
+{
+  protected int x;
+
+  abstract int af(int y);
+  int cf(int y) { return x + y; }
+}
---
+++ _QuickExperiments/JAIPerspectiveTransform.java
@@ -0,0 +1,97 @@
+//package pers;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.NoninvertibleTransformException;
+import java.awt.image.BufferedImage;
+import java.awt.image.renderable.ParameterBlock;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.media.jai.JAI;
+import javax.media.jai.PerspectiveTransform;
+import javax.media.jai.PlanarImage;
+import javax.media.jai.RenderedOp;
+import javax.media.jai.WarpPerspective;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class JAIPerspectiveTransform extends JFrame
+{
+  static class Panel extends JPanel
+  {
+    @Override
+    protected void paintComponent(Graphics g)
+    {
+      Graphics2D g2d = (Graphics2D) g;
+      try
+      {
+        BufferedImage image = GetImage();
+        g2d.drawImage(image, 20, 20, null);
+        g2d.drawImage(a(image), 20 + image.getWidth() + 10, 20, null);
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+      }
+      catch (NoninvertibleTransformException e)
+      {
+        e.printStackTrace();
+      }
+      catch (CloneNotSupportedException e)
+      {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  static BufferedImage GetImage() throws IOException
+  {
+    BufferedImage src = ImageIO.read(JAIPerspectiveTransform.class.getResource("TestImage.jpg"));
+    BufferedImage image = new BufferedImage(src.getWidth(), src.getHeight(),
+        BufferedImage.TYPE_INT_ARGB_PRE);
+    Graphics2D g2d = (Graphics2D) image.getGraphics();
+    g2d.drawImage(src, 0, 0, null);
+
+    return image;
+  }
+
+  public static BufferedImage a(BufferedImage image)
+      throws NoninvertibleTransformException, CloneNotSupportedException
+  {
+    int w = image.getWidth();
+    int h = image.getHeight();
+
+    // Creates a PerspectiveTransform that maps an arbitrary quadrilateral onto another arbitrary quadrilateral.
+    PerspectiveTransform transform = PerspectiveTransform.getQuadToQuad(
+        // x0, y0, x1, y1, x2, y2, x3, y3
+        0, 0, w, 0, w, h, 0, h,
+        // x0p, y0p, x1p, y1p, x2p, y2p, x3p, y3p
+        0, 0, w, 50, w, h - 50, 0, h
+    );
+
+    ParameterBlock pb = new ParameterBlock();
+    pb.addSource(image);
+    pb.add(new WarpPerspective(transform.createInverse()));
+
+    RenderedOp renderedOp = JAI.create("warp", pb);
+    PlanarImage planarImage = renderedOp.createInstance();
+
+    return planarImage.getAsBufferedImage();
+  }
+
+  public static void main(String[] args)
+  {
+    SwingUtilities.invokeLater(new Runnable()
+    {
+        public void run()
+        {
+          JAIPerspectiveTransform frame = new JAIPerspectiveTransform();
+          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          frame.getContentPane().add(new Panel());
+          frame.setSize(800, 600);
+          frame.setVisible(true);
+        }
+    });
+  }
+}
