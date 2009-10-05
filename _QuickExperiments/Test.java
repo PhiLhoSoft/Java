3f7718011b5b3f7d7853b989085c78d191222b5d
PhiLho <PhiLho@GMX.net>
2009-10-05 18:59:05 +0200

    Some tricks

--- _QuickExperiments/Test.java
+++ _QuickExperiments/Test.java
@@ -1,22 +1,31 @@
-import java.util.regex.*;
+import java.util.*;
 
 class Test
 {
-  static final String testString = "foo\nbar\n[code]\nprint'';\nprint{'c'};\n[/code]\nbar\nfoo";
-  static final String replaceString = "<br>\n";
   public static void main(String args[])
   {
-    Pattern p = Pattern.compile("(.+?)(\\[code\\].*?\\[/code\\])?", Pattern.DOTALL);
-    Matcher m = p.matcher(testString);
-    StringBuilder result = new StringBuilder();
-    while (m.find()) 
+    int[] solutionArray = { 1, 2, 3, 4, 5, 6, 16, 15, 14, 13, 12, 11 };
+
+    shuffleArray(solutionArray);
+    for (int i = 0; i < solutionArray.length; i++)
     {
-      result.append(m.group(1).replaceAll("\\n", replaceString));
-      if (m.group(2) != null)
-      {
-        result.append(m.group(2));
-      }
+      System.out.print(solutionArray[i] + " ");
     }
-    System.out.println(result.toString());
+    System.out.println("");
+  }
+
+  // Implementing [Fisher–Yates shuffle](http://en.wikipedia.org/wiki/Fisher-Yates_shuffle)
+  static void shuffleArray(int[] ar)
+  {
+    Random rnd = new Random();
+    for (int i = ar.length - 1; i >= 0; i--)
+    {
+      int index = rnd.nextInt(i + 1);
+      // Simple swap
+      int a = ar[index];
+      ar[index] = ar[i];
+      ar[i] = a;
+    }
   }
 }
+
