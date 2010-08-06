fe6c22431caa8040a132b9393969b3e0bdadaf90
PhiLho <PhiLho@GMX.net>
2010-08-06 18:45:17 +0200

    Improving and finishing the code to run an external application

--- _QuickExperiments/RunApp/StreamGobbler.java
+++ _QuickExperiments/RunApp/StreamGobbler.java
@@ -1,6 +1,3 @@
-package network.myproptima.synchronisation;
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,13 +14,16 @@ public class StreamGobbler extends Thread
 	String m_type;
 	String m_processName;
 
+	public StreamGobbler()
+	{
+		m_type = m_processName = "";
+	}
 	public StreamGobbler(InputStream is, String processName)
 	{
 		m_is = is;
 		m_type = "";
 		m_processName = processName;
 	}
-
 	public StreamGobbler(InputStream is, String type, String processName)
 	{
 		m_is = is;
@@ -31,8 +31,24 @@ public class StreamGobbler extends Thread
 		m_processName = processName;
 	}
 
+	public void SetInputStream(InputStream is)
+	{
+		m_is = is;
+	}
+	public void SetType(String type)
+	{
+		m_type = type;
+	}
+	public void SetProcessName(String processName)
+	{
+		m_processName = processName;
+	}
+
+	@Override
 	public void run()
 	{
+		if (m_is == null)
+			throw new IllegalStateException("No input stream defined!");
 		InputStreamReader isr = new InputStreamReader(m_is);
 		BufferedReader br = new BufferedReader(isr);
 		String line = null;
@@ -47,9 +63,23 @@ public class StreamGobbler extends Thread
 		{
 			ioe.printStackTrace();
 		}
+		finally
+		{
+			if (br != null)
+			{
+				try
+				{
+					br.close();
+				}
+				catch (IOException ioe)
+				{
+					ioe.printStackTrace();
+				}
+			}
+		}
 	}
 
-	public String HandleOutputLine(String line)
+	protected String HandleOutputLine(String line)
 	{
 		// Default implementation, to override
 		String l = m_processName + " " + m_type + "> " + line;
