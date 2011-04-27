5b719aeb8bb4b7dc3b14ca75559eea5c3d83fe69
PhiLho <PhiLho@gmx.net>
2011-04-27 17:53:12 +0200

    Getting there with Json parser... Still need some work.

--- _QuickExperiments/ParseJsonTest.java
+++ _QuickExperiments/ParseJsonTest.java
@@ -7,8 +7,8 @@ import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.JsonToken;
 
-// javac -cp C:\Java\libraries\jackson-core-asl-1.7.2.jar ParseJsonTest.java
-// java -cp .;C:\Java\libraries\jackson-core-asl-1.7.2.jar ParseJsonTest
+// javac -cp C:\Java\libraries\jackson-core-asl-1.7.6.jar ParseJsonTest.java
+// java -cp .;C:\Java\libraries\jackson-core-asl-1.7.6.jar ParseJsonTest
 class ParseJsonTest
 {
   public static void main(String args[])
@@ -17,8 +17,8 @@ class ParseJsonTest
     if (args.length == 0)
     {
       // Default to a known file
-//~       fileName = "D:/Dev/PhiLhoSoft/Processing/_QuickExperiments/_XML_JSON/UsingJackson/data/weather.json";
-      fileName = "H:/PhiLhoSoft/Processing/_QuickExperiments/_XML_JSON/UsingJackson/data/weather.json";
+      fileName = "D:/Dev/PhiLhoSoft/Processing/_QuickExperiments/_XML_JSON/UsingJackson/data/weather.json";
+//~       fileName = "H:/PhiLhoSoft/Processing/_QuickExperiments/_XML_JSON/UsingJackson/data/weather.json";
     }
     else
     {
@@ -31,9 +31,10 @@ class ParseJsonTest
     {
       temperatures = pw.parse("data/weather:0/hourly/@waterTemp_C");
     }
-    catch (ParseException pe)
+    catch (IllegalArgumentException ex)
     {
-      pe.printStackTrace();
+      System.out.println("Incorrect path!");
+      System.exit(-1);
     }
     if (temperatures != null)
     {
@@ -41,6 +42,10 @@ class ParseJsonTest
       {
         System.out.println("* " + temp);
       }
+    }
+    else
+    {
+      System.out.println("Data not found.");
     }
   }
 }
@@ -52,6 +57,7 @@ class ParseWeather
   String[] m_path;
   int m_currentJsonLevel;
   int m_currentPathLevel;
+  ArrayList<String> m_elements = new ArrayList<String>();
 
   public ParseWeather(String fileName)
   {
@@ -66,27 +72,19 @@ class ParseWeather
     }
   }
 
-  public String[] parse(String path) throws ParseException
+  public String[] parse(String path)
   {
-    int pos = path.indexOf("@");
-    if (pos < 0)
-      throw new ParseException("Missing attribute in the path", pos);
-
-    String attribute = path.substring(pos + 1);
-    path = path.substring(0, pos - 1);
-    println("Finding " + attribute + " in " + path);
-
-    String[] result = null;
     try
     {
-      if (goToPath(path))
+      if (!goToPath(path))
       {
-        result = null; // TODO
+        return null;
       }
     }
     catch (IOException ioe)
     {
       println("Cannot read file: " + ioe);
+      return null;
     }
     finally
     {
@@ -100,18 +98,21 @@ class ParseWeather
       catch (IOException ioe)
       {
         println("Cannot close file: " + ioe);
+        return null;
       }
     }
-    return result;
+    String[] result = new String[m_elements.size()];
+    return m_elements.toArray(result);
   }
 
-  // Path is a simple path of intermediary nodes whole values are objects, and a final node (any kind),
+  // Path is a simple path of intermediary nodes whole values are objects, and a final node (attribute),
   // with the names separated by a slash.
   boolean goToPath(String path) throws IOException
   {
     PathHandler ph = new PathHandler(path);
 
     boolean bInArray = false;
+    boolean bCollectValue = false;
     int level = 0;
     int currentArrayIndex = -1;
     int targetArrayIndex = -1;
@@ -131,7 +132,7 @@ class ParseWeather
       {
         // End of Json data
         println("End...");
-        return false;
+        return true;
       }
       println("Token: " + token);
       switch (token)
@@ -151,10 +152,37 @@ class ParseWeather
           currentArrayIndex = 0;
           break;
         case FIELD_NAME:
-          println("Name: " + m_parser.getCurrentName());
+          String name = m_parser.getCurrentName();
+          println("Name: " + name);
+          if (ph.isAttribute(name))
+          {
+            if (level != ph.getAttributeLevel())
+            {
+              println("Almost! Found at " + level + ", expected at " + ph.getAttributeLevel());
+            }
+            else
+            {
+              println("FOUND!");
+              bCollectValue = true;
+            }
+          }
+          /*
+          if (level == ph.getLevel() && ph.isAttribute(name))
+          {
+            println("FOUND!");
+            bCollectValue = true;
+          }
+          */
           break;
         case VALUE_STRING:
-          println("Value: " + m_parser.getText());
+          String value = m_parser.getText();
+          println("Value: " + value);
+          if (bCollectValue)
+          {
+            println("GOT!");
+            bCollectValue = false;
+            m_elements.add(value);
+          }
           break;
         case END_OBJECT:
           level--;
@@ -169,7 +197,7 @@ class ParseWeather
           println("Unexpected token: " + token);
       }
     } while (token != null);
-    return false;
+    return true;
   }
 
   /*
@@ -187,12 +215,21 @@ class PathHandler
   private String m_path;
   /** The parts of the path. */
   private PathPart[] m_parts;
+  /** The attribute to find. */
+  private String m_attribute;
   /** Index used by the iterator. */
   private int m_index;
 
   public PathHandler(String path)
   {
-    m_path = path;
+    int pos = path.indexOf("@");
+    if (pos < 0)
+      throw new IllegalArgumentException("Missing attribute in the path");
+
+    m_attribute = path.substring(pos + 1);
+    m_path = path.substring(0, pos - 1);
+    System.out.println("Finding " + m_attribute + " in " + m_path);
+
     String[] nodes = path.split("/");
     m_parts = new PathPart[nodes.length];
 
@@ -229,9 +266,21 @@ class PathHandler
   {
     return m_parts[m_index].m_pos;
   }
+  public int getLevel()
+  {
+    return m_index;
+  }
   public boolean isCurrentAnArray()
   {
     return m_parts[m_index].m_pos >= 0;
+  }
+  public boolean isAttribute(String name)
+  {
+    return name.equals(m_attribute);
+  }
+  public int getAttributeLevel()
+  {
+    return m_parts.length + 1;
   }
 
   private PathPart parseName(String name)
