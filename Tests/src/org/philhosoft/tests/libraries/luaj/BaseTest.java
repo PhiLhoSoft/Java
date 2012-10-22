13ca3750f4345e8c22862d3b29f6eae86058ae86
PhiLho <PhiLho@gmx.net>
2012-10-22 14:44:46 +0200

    Forgot ResourceUtil from previous commit...; also improved luaj test

--- Tests/src/org/philhosoft/tests/libraries/luaj/BaseTest.java
+++ Tests/src/org/philhosoft/tests/libraries/luaj/BaseTest.java
@@ -14,12 +14,17 @@ Copyright (c) 2012 Philippe Lhoste / PhiLhoSoft
 */
 package org.philhosoft.tests.libraries.luaj;
 
+import java.util.Arrays;
+
 import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 import org.luaj.vm2.*;
 import org.luaj.vm2.lib.jse.*;
+
+import org.philhosoft.util.ResourceUtil;
 
 
 /**
@@ -31,18 +36,58 @@ import org.luaj.vm2.lib.jse.*;
  */
 public final class BaseTest
 {
+	// Create an environment to run in
+	private static Globals _G = JsePlatform.standardGlobals();
+
+	public static ScriptEngine getLuaScriptEngine()
+	{
+		ScriptEngineManager mgr = new ScriptEngineManager();
+		ScriptEngine se = mgr.getEngineByExtension(".lua");
+		return se;
+	}
+
+	public static void showInformation()
+	{
+		ScriptEngine engine = getLuaScriptEngine();
+        ScriptEngineFactory factory = engine.getFactory();
+
+        System.out.println("Engine Name: " + factory.getEngineName());
+        System.out.println("Engine Version: " + factory.getEngineVersion());
+        System.out.println("Names: " + factory.getNames());
+        System.out.println("Language Name: " + factory.getLanguageName());
+        System.out.println("Language Version: " + factory.getLanguageVersion());
+        System.out.println("Extensions: " + factory.getExtensions());
+        System.out.println("Mime Types: " + factory.getMimeTypes());
+
+        String statement = factory.getOutputStatement("'Hello World!'");
+        System.out.println(statement);
+        try
+		{
+			engine.eval(statement);
+		}
+		catch (ScriptException e)
+		{
+			e.printStackTrace();
+		}
+
+        String call = factory.getMethodCallSyntax("someString", "rep", "5");
+        System.out.println(call);
+	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args)
 	{
+		BaseTest bt = new BaseTest();
+		showInformation();
+
+		// Examples from http://luaj.org/luaj/README.html
+
+
 		// Run a script in a Java Application
 
-		String scriptPath = "org/philhosoft/tests/libraries/luaj/hello.lua";
-		LuaValue script = LuaValue.valueOf(scriptPath);
-
-		// Create an environment to run in
-		Globals _G = JsePlatform.standardGlobals();
+		String scriptPath = ResourceUtil.getClassPath(bt) + "hello.lua";
 
 		// Use the convenience function on the globals to load a chunk.
 		LuaValue chunk = _G.loadFile(scriptPath);
@@ -50,23 +95,34 @@ public final class BaseTest
 		// Use any of the "call()" or "invoke()" functions directly on the chunk.
 		chunk.call();
 
-		// Alternative
+		// Simpler alternative...
+		LuaValue script = LuaValue.valueOf(scriptPath);
 		_G.get("dofile").call(script);
 
 
 		// Run a script using JSR-223 Dynamic Scripting
 
-		ScriptEngineManager mgr = new ScriptEngineManager();
-		ScriptEngine se = mgr.getEngineByExtension(".lua");
+		ScriptEngine se = getLuaScriptEngine();
 		se.put("x", 25);
 		try
 		{
-			se.eval("y = math.sqrt(x)");
+			se.eval("y1 = math.sqrt(x)");
+			se.eval("y2 = math.sqrt(-x)");
+			se.eval("y3 = x / 0");
+			se.eval("y4 = xx * x");
 		}
 		catch (ScriptException e)
 		{
 			e.printStackTrace();
 		}
-		System.out.println("y=" + se.get("y"));
+		catch (LuaError e)
+		{
+			System.out.println("Error when running Lua script:");
+			e.printStackTrace();
+		}
+		System.out.println("y1 = " + se.get("y1"));
+		System.out.println("y2 = " + se.get("y2"));
+		System.out.println("y3 = " + se.get("y3"));
+		System.out.println("y4 = " + se.get("y4"));
 	}
 }
---
+++ Utilities/src/org/philhosoft/util/ResourceUtil.java
@@ -0,0 +1,47 @@
+/*
+ * org.philhosoft.*: A collection of utility classes for Java.
+ */
+/* File history:
+ *  0.01.000 -- 2012/10/21 (PL) -- Creation
+ */
+/*
+Author: Philippe Lhoste <PhiLho(a)GMX.net> http://Phi.Lho.free.fr
+Copyright notice: For details, see the following file:
+http://Phi.Lho.free.fr/softwares/PhiLhoSoft/PhiLhoSoftLicense.txt
+This program is distributed under the zlib/libpng license.
+Copyright (c) 2012 Philippe Lhoste / PhiLhoSoft
+*/
+package org.philhosoft.util;
+
+public class ResourceUtil
+{
+	/**
+	 * Gives the class path of the given object, ie. for an instance of
+	 * org.philhosoft.foo.bar.Baz, gives "org/philhosoft/foo/bar/".
+	 * (Slashes as file separator work well in most systems, including Windows.)
+	 *
+	 * @param obj  the object for which we want the path of the .class file
+	 * @return the path, with a final slash
+	 */
+	public static String getClassPath(Object obj)
+	{
+		String cp = getPackage(obj).replaceAll("\\.", "/");
+		return cp;
+	}
+
+	/**
+	 * Gives the package of the given object, ie. for an instance of
+	 * org.philhosoft.foo.bar.Baz, gives "org.philhosoft.foo.bar.".
+	 *
+	 * @param obj  the object providing the package part
+	 * @return the package, with a final dot
+	 */
+	public static String getPackage(Object obj)
+	{
+		String pkg = obj.getClass().getCanonicalName();
+		int pos = pkg.lastIndexOf("."); // Slash before the class name
+		if (pos == -1)
+			return ""; // No package
+		return pkg.substring(0, pos + 1); // Keep the ending dot
+	}
+}
